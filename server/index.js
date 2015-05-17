fa4847939a2fe94a4193bded28747d5c6713775e
Tim Branyen <tim@tabdeveloper.com>
2015-05-17 15:37:16 -0400

    Remove API versioning as this is middleware

--- server/index.js
+++ server/index.js
@@ -7,8 +7,5 @@ const email = require('./email');
 // This processes the queue as necessary.
 email.processQueue();
 
-var app = configure(express());
-
-app.use('/api/v1/', api);
-
-module.exports = app;
+// Export the API as middleware.
+module.exports = configure(express());
