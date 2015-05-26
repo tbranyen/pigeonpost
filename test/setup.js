43011248b50bcbce511a2a6d8eb0ade90e0a6c83
Tim Branyen <tim@tabdeveloper.com>
2015-05-26 11:16:04 -0400

    Minor cleanup commit for tests

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,9 @@
+var path = require('path');
+
+// Configure test environment.
+process.env.AWS_SES_SECRETS = path.join(__dirname, 'fixtures/secrets.json');
+
+// Install mocks.
 [
   'request',
   'aws-sdk',
